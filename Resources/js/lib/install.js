a10cbeb820b73f8ef21e8e52f8d240707b1be68f
Xavier Lacot <xavier@lacot.org>
2011-03-16 18:25:24 +0100

    fixed version number

--- Resources/js/lib/install.js
+++ Resources/js/lib/install.js
@@ -1,6 +1,6 @@
 // increment the number below each time you push a new model version
 // (forces rebuild on phone)
-var current_migration_version = 6;
+var current_migration_version = 1;
 
 // create the tables if required
 joli.models.migrate(current_migration_version);
